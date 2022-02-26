4510d140d749cf6b4c286f11d13bcfbc9d1bd90b
hibiki-ng <valentin.engelhard@eemi.com>
2022-02-26 17:45:13 +0100

    Functional weather API requests

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -12,26 +12,31 @@ class WeatherController extends AbstractController
     /**
      * @Route("/weather", name="app_weather")
      */
-    public function index(): Response
-    {
-        return $this->render('weather/index.html.twig', [
-            'controller_name' => 'WeatherController',
-        ]);
-    }
-
-    public function test(HttpClientInterface $httpClient)
+    public function index(HttpClientInterface $httpClient):Response
     {
         $request = $httpClient->request(
             'GET',
             'https://goweather.herokuapp.com/weather/Paris');
         
         if ( $request->getStatusCode() === 200 ) {
-            return $request->toArray();
-            echo 'ok';
+            $content = $request->toArray();
+
+            $temp = $content["temperature"];
+            $weather = $content["description"];
+            $meteo;
+            if ($weather == "Sunny") {
+                $meteo = "Soleil";
+            }
+            else {
+                $meteo = "Moins beau temps";
+            }
         } else {
             echo 'error';
         }
-        $request->getContent();
-        return $request->toArray();
+
+        return $this->render('weather/index.html.twig', array(
+            'temp' => $temp,
+            'meteo' => $meteo
+        ));
     }
 }
